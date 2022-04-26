88dd1ac1e56a1db86f3e6523708462ba3aa2d406
Marčelo Pavić <vu00422@vestigo.hr>
2022-04-26 09:41:21 +0200

    Initial commit

---
+++ src/main/java/com/rest/restapihnb/RestApiHnbApplication.java
@@ -0,0 +1,13 @@
+package com.rest.restapihnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiHnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiHnbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/restapihnb/RestApiHnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.restapihnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiHnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
