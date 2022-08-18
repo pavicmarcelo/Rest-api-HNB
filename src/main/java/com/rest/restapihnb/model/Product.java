04925c6d4d50316a567017522a93fca2e9bef1c7
Marčelo Pavić <vu00422@vestigo.hr>
2022-08-18 14:30:28 +0200

    ispravak Product klase, dodan novi konstruktor

--- src/main/java/com/rest/restapihnb/model/Product.java
+++ src/main/java/com/rest/restapihnb/model/Product.java
@@ -1,21 +1,13 @@
 package com.rest.restapihnb.model;
 
-
-
-import com.rest.restapihnb.controller.ExchangeHNBController;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
-import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 
 
@@ -38,30 +30,14 @@ public class Product {
     private BigDecimal price_hrk;
     private BigDecimal price_eur;
     private String description;
-   // private Boolean isAvailable;
 
 
-
-/*
-    public Product (String code, String name, BigDecimal price_hrk, String description, Boolean isAvailable) {   // izbaciti
-
-        ExchangeHNBController exchangeHNBController = new ExchangeHNBController();
-
+    public Product (String code, String name, BigDecimal price_hrk, String description) {
         this.code = code;
         this.name = name;
         this.price_hrk = price_hrk.setScale(2, RoundingMode.HALF_EVEN);
-        try {
-            this.price_eur = price_hrk.divide(exchangeHNBController.getSrednjiTecajFromHNBApi(), 2, RoundingMode.CEILING);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
         this.description = description;
-        this.isAvailable = isAvailable;
     }
-
-*/
-
-
 
 
 }
